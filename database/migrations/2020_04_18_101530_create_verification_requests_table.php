da657cb92f3672b221346c060ee24a809c825475
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-04-18 16:17:17 +0530

    verification requests migration and routes

---
+++ database/migrations/2020_04_18_101530_create_verification_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVerificationRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification_requests', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('phone_number', 20);
+            $table->string('ext', 10);
+            $table->boolean('is_verified');
+            $table->dateTime('expires_at');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification_requests');
+    }
+}
