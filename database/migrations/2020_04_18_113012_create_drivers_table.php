8c2e34c4614cbbcc90c0a1ca6d886fd33251e99d
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-04-18 17:02:50 +0530

    Drivers, Order status, Waypoint status, Recipient migration and models

---
+++ database/migrations/2020_04_18_113012_create_drivers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 50);
+            $table->string('profile_pic', 500);
+            $table->string('contact', 250);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+}
